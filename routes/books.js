d896b4f1e02fc7d91f9435617f4feb96c395b303
Jardel Pereira Zermiani <32064166+Jardelpz@users.noreply.github.com>
2020-05-06 00:25:43 -0300

    Image books

--- routes/books.js
+++ routes/books.js
@@ -21,8 +21,21 @@ const upload = multer({ //fileffilter to do
 
 //All books
 router.get('/', async (req, res)=>{
+    let query = Book.find();
+    if(req.query.title != null && req.query.title != ''){
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+
+    if(req.query.publishedBefore != null && req.query.publishedBefore != ''){
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+
+    if(req.query.publishedAfter != null && req.query.publishedAfter != ''){
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    //less then equal e greater than equal
     try{
-        const books = await Book.find({})
+        const books = await query.exec();
         res.render('books/index', {
             books: books,
             searchOptions: req.query
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@
 
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req, res)=>{
-    res.render('index')
+router.get('/', async (req, res)=>{
+    let books
+    try{
+        books = await Book.find().sort({ createdAt : 'desc'}).limit(5).exec() // os 5 ultimos livros cadastrados
+    }catch(err){
+        books = []
+    }
+    console.log(books)
+    res.render('index', {
+        books: books
+    })
 })
 
 
